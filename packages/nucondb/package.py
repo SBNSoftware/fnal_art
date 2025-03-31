ff2e6a6d6d557a4e3a6e4b4c9e691f5603994409
Marc Mengel <mengel@fnal.gov>
2025-03-31 14:18:20 -0500

    newcondb moved to github

--- packages/nucondb/package.py
+++ packages/nucondb/package.py
@@ -9,15 +9,20 @@ from spack.package import *
 class Nucondb(MakefilePackage):
     """Data handling client code for intensity frontier experiments"""
 
-    homepage = "https://cdcvs.fnal.gov/redmine/projects/nucondb"
-    url = "https://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/nucondb.v2_2_10.tbz2"
-    git = "https://cdcvs.fnal.gov/projects/ifdhc-nucondb"
+    homepage = "https://github.com/fnal-fife/nucondb"
+    git_base = "https://github.com/fnal-fife/nucondb.git"
+    url = "https://github.com/fnal-fife/nucondb/archive/refs/tags/v2_6_14.tar.gz"
+    list_url = "https://github.com/fnal-fife/nucondb/tags"
 
-    version("2.6.1", sha256="fbf59259e8cd75334b4ad17d3da46519d35c5c81ccf55b8df2d19f664fe016b9")
-    version("2.5.23", sha256="30399133b64548affd3522eb16346a1fc85f36ba4300fce51730a208a8022f24")
-    version("2.5.22", sha256="bd468d5b64909e3951342eacef30440e1eaca5d32add991fe6cc36b46f1c5f65")
-    version("2.5.17", sha256="5b9bd0e8e6359edb594416847a946214376f013afedebbd861ebd6556d45823f")
-    version("2.5.16", sha256="2de3a199407a378ea62f76652bc0945065d6636c7f7a02e5fafa9667f4ae833a")
+    version("2_6_2", sha256="3fffd0d4291c76dbba0f94c75d678057ae12d1a36983c8cccd7098158f7b6874")
+    version("2_6_1", sha256="0fd793361322224c74c413dc105fb53f60488a6234c5ad4f31843e6b4462d39e")
+    version("2_6", sha256="e205a12c28466bdf32ec4373a76877e28606f6ed33f58d1d64c738ac03d232d7")
+    version("2_5_23", sha256="eb8c0b9720ffcc0e2146ee704349f7ce41748ca91614aeb9644ec388c4f7ad4d")
+    version("2_5_22", sha256="583917373cedce05150d892541402332795444ec019d961870807150da540079")
+    version("2_5_21", sha256="20c19e30ac6eda4df5058d2ca6753ad6c0de25c80df495b5fa97910966af7140")
+    version("2_5_20", sha256="1395d43c3bb954bf0982b85a1f49ee6a9454924f2d403f74a35be75c19f101c9")
+    version("2_5_19", sha256="73d589ba9437c40e990c7b872f8ff5154e32697f9e1990d6253d898ed11b810f")
+    version("2_5_18", sha256="f1ed5f66aa373292eb917d09885f2f2fbc41385521fb396050d441e300508f2a")
     version("2.5.2", tag="v2_5_2", get_full_repo=True)
     version("2.4.8", tag="v2_4_8", get_full_repo=True)
     version("2.3.0", tag="v2_3_0", get_full_repo=True)
