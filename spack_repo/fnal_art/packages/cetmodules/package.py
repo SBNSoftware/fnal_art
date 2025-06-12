b73dc8f9070cc880f6f3e11d1943e7931ae974df
Patrick Gartung <gartung@fnal.gov>
2025-06-12 12:43:14 -0500

    Migration to v2

---
+++ spack_repo/fnal_art/packages/cetmodules/package.py
@@ -0,0 +1,111 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack.package import *
+
+
+class Cetmodules(CMakePackage):
+    """CMake glue modules and scripts required by packages originating at
+    Fermilab and associated experiments and other collaborations.
+    """
+
+    homepage = "https://fnalssi.github.io/cetmodules/"
+    git = "https://github.com/FNALssi/cetmodules.git"
+    url = "https://github.com/FNALssi/cetmodules/archive/refs/tags/3.13.02.tar.gz"
+
+    maintainers = ["chissg", "gartung", "marcmengel", "vitodb"]
+
+    version("develop", branch="develop", get_full_repo=True)
+
+    version("3.27.02", sha256="b6d902b283ef1d7a7ad46da1356826427a1957228ffe8cb7d7fb063ce7ca6d30")
+    version("3.27.01", sha256="5e64b874eb4953ad62c0de4ae8c279f0638e4a13b8c9515e50890c9952a73ee9")
+    version("3.27.00", sha256="6926fd8df351713bce0382ab57760d398437687e24da40bbd1306d6b17d86661")
+    version("3.26.00", sha256="0acf1a916cb378dba819dd954de1e5279db5053658827844886006466cec3dc8")
+    version("3.25.00", sha256="9574d9f8e5757d79c11d6fb3b95c3227aa0028960733715c3e6b6b25a51ff5c5")
+    version("3.24.01", sha256="6ef8b522b02298a787a318cda898deedc250cdd336dc3168fad934ef607b5916")
+    version("3.22.02", sha256="bbd6a80ab3c495e49b3545230c97ae880755a5f1d39b71d89f07297bf835e5da")
+    version("3.22.01", sha256="c72c47328adc0c95f905aae119c76d35513a0677f20163f0ef25a82bd0f72082")
+    version("3.21.02", sha256="255d6d6c2455217734b208fc90919b90bc7c0f9a59a4706d329c642bff51f004")
+    version("3.21.01", sha256="9f4b845f9ed09fb3a8ee7864ac487afd08a5b3e64abf394831ee927f91b08ebc")
+    version("3.21.00", sha256="429ddecf2e905a6a3156c267005d17cd6e160533f28bcef0be40a9d0057e95e4")
+    version("3.19.02", sha256="214172a59f4c3875a5d7c2617b9f50ed471c86404d85e2e5c72cadf5b499cdc6")
+    version("3.13.02", sha256="11bc4b55a3b07dfe1187d3f04c977caec9fb06d412ed50241f30507398cc7cac")
+
+    variant(
+        "versioned-docs", default=False, description="build versioned docs with a landing page"
+    )
+    variant("docs", default=False, when="~versioned-docs", description="build documentation")
+
+
+    def patch(self):
+        if self.spec.satisfies("@3.26.00:3.27.02"):
+            filter_file(
+                r"    _include(${ARGV} OPTIONAL)",
+                "    list(APPEND ARGV OPTIONAL)\n    list(REMOVE_DUPLICATES ARGV)\n    _include(${ARGV})",
+                "Modules/compat/art/CetCMPCleaner.cmake",
+                string=True,
+            )
+
+    depends_on("cmake@3.20:", when="@3.03.00:", type=("build", "run"))
+    depends_on("cmake@3.21:", when="@3.22.02:", type=("build", "run"))
+    depends_on("cmake@3.22:", when="@3.23.00:", type=("build", "run"))
+
+    with when("+versioned-docs") or when("+docs"):
+        depends_on("git@2.22:", type="build")
+        depends_on("py-sphinxcontrib-moderncmakedomain", type="build")
+        depends_on("py-sphinx-design@0.2.0:", type="build")
+        depends_on("py-sphinx@5:", type="build")
+
+        with when("@3.23.00:"):
+            depends_on("py-sphinxcontrib-moderncmakedomain@3.25:", type="build")
+            depends_on("py-sphinx@6:", type="build")
+            with when("+versioned-docs"):
+                depends_on("py-sphinx-toolbox", type="build")
+                depends_on("py-sphinxcontrib-jquery", type="build")
+
+        with when("@:3.22.99"):
+            depends_on("py-sphinx@:5", type="build")
+
+    depends_on("perl", type=("build", "test"))
+
+    conflicts("@:3.19.01", when="^cmake@3.24.0:")
+
+    if "SPACK_CMAKE_GENERATOR" in os.environ:
+        generator = os.environ["SPACK_CMAKE_GENERATOR"]
+        if generator.endswith("Ninja"):
+            depends_on("ninja@1.10:", type="build")
+
+    @run_before("cmake")
+    def fix_fix_man(self):
+        filter_file(r"exit \$status", "exit 0", "%s/libexec/fix-man-dirs" % self.stage.source_path)
+
+    def cmake_args(self):
+        spec = self.spec
+        define = self.define
+        options = []
+        if spec.satisfies("@:3"):
+            options.extend(["--preset", "default"])
+        if any(
+            [
+                spec.variants[doc_opt].value
+                for doc_opt in ("docs", "versioned-docs")
+                if doc_opt in spec.variants
+            ]
+        ):
+            options.append(define("BUILD_DOCS", True))
+        if spec.variants["versioned-docs"].value:
+            options += [
+                define(
+                    f"{self.name}_SPHINX_DOC_PUBLISH_VERSION_BRANCH", spec.version
+                ),
+                define(
+                    f"{self.name}_SPHINX_DOC_PUBLISH_ROOT",
+                    join_path(self.stage.path, "doc_root"),
+                ),
+            ]
+        return options
---
+++ spack_repo/fnal_art/packages/ifdhc_config/package.py
@@ -0,0 +1,66 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+import llnl.util.tty as tty
+
+from spack_repo.builtin.build_systems.generic import Package
+from spack.package import *
+
+
+class IfdhcConfig(Package):
+    """Config package for Data handling client code for intensity frontier experiments"""
+
+    homepage = "https://github.com/fnal-fife/ifdhc"
+    git_base = "https://github.com/fnal-fife/ifdhc.git"
+    url = "https://github.com/fnal-fife/ifdhc/archive/refs/tags/v2_6_14.tar.gz"
+    list_url = "https://github.com/fnal-fife/ifdhc/tags"
+
+    version("2.8.0", sha256="189404744961ca049366369b71b783e57540cd4f6a45b86f0aed5f14c198d590")
+    version("2.7.4", sha256="940dc661cfb5a1bf9bf7353b03b0fd732289a951ef99992327f29ce94f1cac9f")
+    version("2.7.3", sha256="8a8caa1d14a0d39c8ccb96460b84cca51540f209535c62cd680243dc08d1db5f")
+    version("2.7.2", sha256="036933c0443a4704f408aea83972954e2af6d933a7ffe61869ac4e6e6fd41256")
+    version("2.7.1", sha256="4494d08c3a7927600bbcee56e65feb024b15e2d510328ec0d2cc0fcefc5cb6a7")
+    version("2.7", sha256="5a4f7c94191bb657415a51c6d73d789f820999b68c8d9c65ec74c845321aa7e2")
+    version("2.6.20", sha256="cd473b3331474a75f87c3251e08c69bb3b0a7c98f53171c5a380049bca1a0cc2")
+    version("2.6.19", sha256="8ce4cb631fb1662e3146a1877e92677859cb2196cf90504e7b4a55811cc45b4b")
+    version("2.6.18", sha256="c8d353ea648177802b2c2e87213a4b5cbffa62cd6b340bc3c6d574402955791c")
+    version("2.6.17", sha256="d4d3614122099f386d9b4e570dcd058a9d6853c96a8ec6beafcbcb8cee4ebb02")
+    version("2.6.14", sha256="df6d78b62ba074aac84281b021037319623e2bc4a90bfb46a7129ebdf3281ec1")
+    version("2.6.8", sha256="3dc0fca76815424a72613df68520dafc08341c9f60c80a766bda647819212d87")
+    version("2.6.6", sha256="8ba4929ecefa5720ea999e39671a48b56a65f5f44ff99f2f473f9cd47277051e")
+    version("develop", git=git_base, branch="develop", get_full_repo=True)
+
+    parallel = False
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+
+    def url_for_version(self, version):
+        url = "https://github.com/fnal-fife/ifdhc/archive/refs/tags/v{0}.tar.gz"
+        return url.format(version.underscored)
+
+    def install(self, a, b):
+        instlist=[
+                ['ifdh.cfg', ''], 
+                ['ifdh/www_cp.sh', '/bin'],
+                ['ifdh/auth_session.sh', '/bin'], 
+                ['ifdh/decode_token.sh', '/bin'],
+               ]
+        mkdir(self.spec.prefix.bin)
+        for fnpair in instlist:
+            install("{0}/{1}".format( self.stage.source_path, fnpair[0]),
+                    "{0}{1}".format( self.spec.prefix, fnpair[1]))
+            tty.warn("installing {0}".format(fnpair[0]))
+
+    def setup_build_environment(self, spack_env):
+        spack_env.set("IFDHC_CONFIG_DIR", self.spec.prefix)
+
+    def setup_run_environment(self, run_env):
+        run_env.prepend_path("PATH", self.spec.prefix.bin)
+        run_env.set("IFDHC_CONFIG_DIR", self.spec.prefix)
+        # save for ifhdc setup_run_environment to use..
+        os.environ["IFDHC_CONFIG_BIN"] = str(self.spec.prefix.bin)
